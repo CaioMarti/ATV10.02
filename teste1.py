0d8ff2abe82793b0c15c83f65714fb831aad8444
Caio Martins Rodrigues <130692458+CaioMarti@users.noreply.github.com>
2025-03-14 18:53:08 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,20 @@
+
+x = 10
+if x > 5:
+    print("X é maior que 5")
+
+y = 3
+if y % 2 == 0:
+    print("Y é par")
+else:
+    print("Y é ímpar")
+
+
+a = 1
+while a <= 5:
+    print(a)
+    a += 1
+
+
+for i in range(1, 6):
+        print(i)
